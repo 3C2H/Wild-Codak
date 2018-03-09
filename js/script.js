e3fc7db44bdeed00e09c7e8f36b53809fa66c72d
SophieMdl <sophie.michaudel@gmail.com>
2018-03-09 11:35:16 +0100

    test current-item

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
+window.onload = function(){
+    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+        event.preventDefault()
+        $(this).ekkoLightbox()
+    });
+    const currentPage = document.body.id
+    const itemMenu = `menu-${currentPage}`
+    const currentItem = document.querySelector(itemMenu)
+    console.log(currentItem)
+    itemMenu.addClassName = 'active'
+    // switch (currentPage){
+    //     case 'home' :
 
+    //     break;
+    //     case 'galery' :
+    //     break;  
+    //     case 'stories' :
+    //     break;  
+    //     case 'contact' :
+    //     break;  
+    // }
+}
