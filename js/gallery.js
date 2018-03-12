ae7f095489510fd2e5e4b2547213baf22e4fae07
SophieMdl <sophie.michaudel@gmail.com>
2018-03-12 10:02:51 +0100

    branche à jour

--- js/gallery.js
+++ js/gallery.js
@@ -1,25 +1,56 @@
 window.addEventListener('load', function () {
-    
+
     //Gestion lightbox
-    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    $(document).on('click', '[data-toggle="lightbox"]', function (event) {
         event.preventDefault()
         $(this).ekkoLightbox()
     });
 
-    //Gestion de la galerie
-    const row = document.querySelector(`#images-wrapper`)
 
-    for (let i = 1; i <= 42; i++) {
-        let div = document.createElement(`div`)
-        div.className = `col-lg-4 col-sm-6 col-12 picture`
-        div.innerHTML = `
-            <figure>
-                <a href="images/${i}.jpg?image=${i}" data-toggle="lightbox" data-gallery="example-gallery" data-max-width="1400" data-max-height="840">
-                    <img src="images/${i}.jpg" class="img-fluid  img-bkg" alt="gallery-image${i}">
-                    <i class="fas fa-plus"></i>
-                </a>
-            </figure>
-        `
-        row.appendChild(div)
+    let windowHeight = window.innerHeight
+    const row = document.querySelector(`#images-wrapper`)
+    const maxItems = 42
+    let itemsToLoad = 12
+    let i = 1;
+    let step = 210
+    //on calcule le nombre d'item en fonction de la hauteur de l'écran (chaque item fait 210px de hauteur) 
+    // var nbItem = Math.round(windowHeight / 210);
+    loadItems(i, itemsToLoad)
+
+    window.addEventListener('scroll', function(){
+        changeValue(i)
+    })
+
+    function changeValue(i){
+        if (itemsToLoad < maxItems)  {
+            if (window.scrollY > step) {
+                i = i + 3
+                step = step + step
+                itemsToLoad = itemsToLoad + 3
+                console.log('load')
+                console.log(i)
+                loadItems(i, itemsToLoad)
+            }
+        }
+        else{
+            window.removeEventListener("scroll", changeValue)
+        }
+    }
+
+    //Gestion de la galerie
+    function loadItems(i, itemsToLoad) {
+        for (i; i <= itemsToLoad; i++) {
+            let div = document.createElement(`div`)
+            div.className = `col-lg-4 col-sm-6 col-12 picture`
+            div.innerHTML = `
+                <figure>
+                    <a href="images/${i}.jpg?image=${i}" data-toggle="lightbox" data-gallery="example-gallery" data-max-width="1400" data-max-height="840">
+                        <img src="images/${i}.jpg" class="img-fluid  img-bkg" alt="gallery-image${i}">
+                        <i class="fas fa-plus"></i>
+                    </a>
+                </figure>
+            `
+            row.appendChild(div)
+        }
     }
 })
